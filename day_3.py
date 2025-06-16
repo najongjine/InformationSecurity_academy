9ec3f54c64a548cb3f530d866f3d869bad38d74c
najongjine <najongjin3@hotmail.com>
2025-06-16 13:23:29 +0900

    .

---
+++ day_3.py
@@ -0,0 +1,93 @@
+"""
+자바 특강
+
+파이썬:
+a=10
+if
+a="dfdd"
+switch
+a=True
+
+=?a={"key1":"value1"}
+
+답은? 
+
+prinf("%d")
+
+for
+[a,b]=[b,c]
+
+a[::2]
+
+
+자바:
+int a=10;
+String b="dd";
+Boolean c=true
+float d=1.1
+
+나머지는 다 클래스
+
+sysout(a+b+c+d)
+
+if(){
+}
+for(int i=0; i<10; i++){
+}
+switch(){
+}
+while()
+
+do{
+dfdfddd 무조건 한번 실행
+}
+while(){
+조건 만족할때만 진입
+}
+
+상속
+추상화
+인터페이스
+생성자
+static
+this
+overriding
+overloading
+
+c 랑 파이썬은
+
+함수 문제 아닌이상, 무조건 위에서 아래로
+
+자바 문제는 절대 그렇게 안나옴
+
+부모, 자식, 오버라이딩, 업케스팅 
+
+
+class Parent{
+    int a=0;
+    Parent(){}
+}
+class Child extend Parent{
+    int a=0;
+    Child(){
+
+    }
+}
+class void main(){
+    Parent parent=new Parent();
+    Child child=new Child();
+    Parent child2=new Child();
+}
+
+
+c언어 에서 제일 어려운데
+포인터 - 주소값이 뭔가요 !!!! xxxxxxxxxxxxxxxxx
+int * a= "dfdfgsdfgdfgdf"
+b=&a  a 랑 맵핑 했구나. 문법오류 생각 말기
+prinf(%s, a)  & 붙여야 같 나오나? 그런거 고민 x 무조건 값 나옴
+
+malloc 계산 필요 없음
+왜냐면, 무조건 정수로 떨어지는거 나올거임
+int [] a={1,2,3}
+int size= malloc(a/10%99)
+"""
