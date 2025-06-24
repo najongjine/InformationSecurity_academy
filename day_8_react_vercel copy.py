ba68bd3765ffd5524717dc63a81fc72c6e4077b5
najongjine <najongjin3@hotmail.com>
2025-06-24 13:10:30 +0900

    .

---
+++ day_8_react_vercel copy.py
@@ -0,0 +1,9 @@
+"""
+https://github.com/najongjine/Information_security_react_academy
+- react 만든후 vercel에 올리기
+"""
+
+
+"""
+
+"""
---
+++ day_9_numpy.py
@@ -0,0 +1,9 @@
+"""
+https://wildojisan.tistory.com/64
+- react 만든후 vercel에 올리기
+"""
+
+
+"""
+
+"""
