c0838c74c45a9e541daa7dd468d8c3fc047c4924
najongjine <najongjin3@hotmail.com>
2025-06-24 15:12:39 +0900

    .

--- day_9_numpy.py
+++ day_9_numpy.py
@@ -63,8 +63,32 @@ color_img = np.zeros((100,100,3))
 color_img[0:50,0:50]=[0.4,0.4,0.2]
 color_img[0:50,50:100]=[0,1,0]
 color_img[50:100,50:100]=[1,1,1]
-print(color_img)
-plt.imshow(color_img)
-plt.title('100*100 color image')
-plt.axis('off')
-plt.show()
+#print(color_img)
+#plt.imshow(color_img)
+#plt.title('100*100 color image')
+#plt.axis('off')
+#plt.show()
+
+"""
+numpy 는 이해 안가도 슬퍼하지 마세요.
+저희가 GPT 모델을 만들지 않는이상, 이해 안가도 괜찬아요
+진짜 AI 응용이나 활용에선 GPT한테 데이터를 컴퓨터가 알아먹을수 있는
+숫자로 만들라고 시키거나, 다른 툴같은거 써요(예: ImageGenerator, Tokenizer)
+"""
+
+a=np.array([1,2,3,4,5,6])
+#print(a.shape)
+
+b=a.reshape(2,3)
+#print(b)
+
+"""
+reshape을 하는 이유:
+인공지능은 수많은 함수들이 있어요
+어떤 함수는 3차원 shape을 원하고
+어떤 함수는 1차원 shape을 원해요
+그래서 거기에 맞춰주기 위해서 reshape을 써요
+
+reshape은 행렬의 모습만 바꾸는거고,
+안의 숫자 데이터는 바꾸지 않아요
+"""
