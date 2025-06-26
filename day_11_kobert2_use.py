1a02c6fbcbef806cd6de7b192ad93f7bf2b73a4a
najongjine <najongjin3@hotmail.com>
2025-06-26 14:43:44 +0900

    .

--- day_11_kobert2_use.py
+++ day_11_kobert2_use.py
@@ -8,9 +8,15 @@ model = SentenceTransformer('snunlp/KR-SBERT-V40K-klueNLI-augSTS')
 user_input="배송은 얼마나 걸려요?"
 user_embedding=model.encode(user_input,convert_to_tensor=True)
 
+# embedding.pt 읽기. 컴퓨터보고 알아서 읽어라
 data=torch.load('question_embeddings.pt')
+
+# 컴퓨터 입장에서 문장
 saved_embeddings=data['embeddings']
+# DB 입장에서 정해준 문장
 saved_ids=data['ids']
+# 사람입장에서 정해준 문장
 saved_questions=data['questions']
 
-print(saved_embeddings)
+print(saved_ids)
+print(saved_questions)
