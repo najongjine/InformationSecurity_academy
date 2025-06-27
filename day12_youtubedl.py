cf73a76af6b489db4f8a34c02b1c24eb1d034d59
najongjine <najongjin3@hotmail.com>
2025-06-27 14:12:27 +0900

    .

--- day12_youtubedl.py
+++ day12_youtubedl.py
@@ -2,5 +2,7 @@
 
 def download_video(url):
     ydl_opts={
-        'format':'bv*[height<=1080][ext=mp4]+ba[ext=m4a]/b[ext=mp4]'
+        'format':'bv*[height<=1080][ext=mp4]+ba[ext=m4a]/b[ext=mp4]',
+        'merge_output_format':'mp4',
+        'outtmpl':'%(title)s.%(ext)s'
     }
