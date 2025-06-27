c752b4630aafa9ea44357b6acc1e8f8f43885885
najongjine <najongjin3@hotmail.com>
2025-06-27 14:16:54 +0900

    .

--- day12_youtubedl.py
+++ day12_youtubedl.py
@@ -1,4 +1,5 @@
 # pip install yt_dlp
+from yt_dlp import YoutubeDL
 
 def download_video(url):
     ydl_opts={
@@ -6,3 +7,12 @@ def download_video(url):
         'merge_output_format':'mp4',
         'outtmpl':'%(title)s.%(ext)s'
     }
+    """
+    YoutubeDL 이 클래스가 파일을 진짜 저장해요. 그래서 제작자가
+    with 붙여서 이 코드 사용 하래요
+    """
+    with YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+
+url = ""
+download_video(url)
