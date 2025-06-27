b30ae177edb6faa14832f8806f71e426c732acca
najongjine <najongjin3@hotmail.com>
2025-06-27 13:04:56 +0900

    .

--- day_11_realesgan.py
+++ day_11_realesgan.py
@@ -1,1 +1,11 @@
 # pip install realesrgan
+
+from PIL import Image
+from realesrgan import RealESRGANer
+import torch
+
+# cuda gpu 있으면 그거쓰고, 없으면 cpu써
+device=torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+model=RealESRGANer(device,scale=4)
+model.load_weights('RealESRGAN_x4.pth')
