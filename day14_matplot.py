021879de82de5d3c3a10e6707c668f1936b1f7c6
najongjine <najongjin3@hotmail.com>
2025-07-01 14:38:33 +0900

    .

--- day14_matplot.py
+++ day14_matplot.py
@@ -215,3 +215,11 @@ plt.imshow() 이미지 보기
 """
 #plt.scatter(x, y)
 #plt.show()
+
+
+y = np.array([35, 25, 25, 15])
+mylabels = ["Apples", "Bananas", "Cherries", "Dates"]
+myexplode = [0.1, 0.0, 0.0, 0]
+
+plt.pie(y, labels = mylabels, explode = myexplode)
+plt.show() 
