648e03c09d5c05aa34f59d90dc3ee01455029db9
najongjine <najongjin3@hotmail.com>
2025-07-07 13:20:10 +0900

    .

---
+++ day19_cnn.py
@@ -0,0 +1,41 @@
+"""
+Scikit - 이건 숫자, 수치등을 가지고 예측하는 모델
+linear regression, logistic regression, 
+XGBoost -> XGRegression
+
+Tensorflow, Keras -
+[0.1,0.01,0.51..] Neural... Network
+10100101 int, string, bool 
+Keras 는 tensorflow 라는 AI 전용 언어를, 사람이 쓸수 있게
+어느정도 쉽게 해준 프레임워크
+
+tensorflow, keras 는 GPU가 필요하긴 한데,
+그렇게 비싼 GPU는 요구하지 않아요
+즉, 살짝 가벼워요
+transformer 가 나오기 전에 유행했었어요
+이놈을 쓰면, scikit 할수 없었던, 이미지, 자연어 처리를
+간단하게 할수 있다
+
+pytorch, transformer - 요거는 나중에
+
+
+
+AI로 뭘 할수있냐
+
+- 회귀 (돈, 금액, 가치 예측)
+* XGBoost
+
+- 분류(이미지)
+* CNN, Vision Transformer
+
+- 객체탐지
+* YOLO
+
+- 언어처리
+- 음성인식
+* LLM(GPT)
+
+- 강화학습
+* DeepLearningCNN || Q-Learning
+
+"""
