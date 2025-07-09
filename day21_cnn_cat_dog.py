e022bb8c81edcd83fcc1759f642ed2fc8a573969
najongjine <najongjin3@hotmail.com>
2025-07-09 13:30:43 +0900

    .

--- day21_cnn_cat_dog.py
+++ day21_cnn_cat_dog.py
@@ -5,4 +5,28 @@
 # 압축 풀기
 !unzip -q cats_and_dogs_filtered.zip
 
+
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,Flatten,Conv2D,MaxPooling2D
+from tensorflow.keras.optimizers import RMSprop
+
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+"""
+
+"""
+학습 시킬 이미지는 굉장히 깨끗해야 해요.
+예를들어서 고양이를 학습시키는데, 사람이 잡고 있는 사람은 잘못 학습이 될수 있어요
+
+컴퓨터는 사진 하나도, 조명이나 밝기가 달라지면 다른놈으로 인색해요
+예를들어서 고양이가 집안에 있을때랑 밖에서 햇빛 쬐는거랑 다른 생명채로 인색해요
+
+이런 한도끝도 없는 문제는 일일히 조명 쬐주고 햋빛 쬐주고 하면 한도끝도 없어요
+
+이걸 해결해 주는 모듈이 있어요 
+ImageDataGenerator
 """
