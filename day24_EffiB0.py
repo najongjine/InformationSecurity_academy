11fd1e8d83090788934169f8cc5f725a29403b76
najongjine <najongjin3@hotmail.com>
2025-07-14 13:35:05 +0900

    .

--- day24_EffiB0.py
+++ day24_EffiB0.py
@@ -15,3 +15,22 @@ plt.xlabel('Epoch')
 plt.ylabel('loss')
 plt.legend()
 plt.show()
+
+
+
+
+import os
+import json
+
+save_dir='/content/drive/Mydrive/my_models/tensorflow_keras'
+save_model_path=os.path.join(save_dir,'EffiB0_test.h5')
+save_label_path=os.path.join(save_dir,'EffiB0_test.json')
+
+if not os.path.exists(save_dir):
+  os.madedirs(save_dir)
+  print(f"폴더 생성됨: {save_dir}")
+
+model.save(save_model_path)
+with open(save_label_path,'w') as f:
+  json.dump(class_list,f)
+  print(f"클래스 이름 저장 완료:{save_label_path}")
