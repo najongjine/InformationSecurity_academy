7182b26ce11adaae901bfd9e64f31d84905959be
najongjine <najongjin3@hotmail.com>
2025-07-14 14:29:12 +0900

    .

---
+++ day24_EffiB0_predict.py
@@ -0,0 +1,18 @@
+from google.colab import drive
+drive.mount('/content/drive')
+
+import numpy as np
+impoty matplotlib.pyplot as plt
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+import json
+
+# 모델 바꾸면 요것도 바꿔줘야함
+from tensorflow.keras.applications.efficientnet import preprocess_input
+
+IMG_HEIGHT=224
+IMG_WIDTH=224
+
+model_path='/content/drive/MyDrive/my_models/tensorflow_keras/EffiB0_test.h5'
+label_path='/content/drive/MyDrive/my_models/tensorflow_keras/EffiB0_test.json
+model=load_model(model_path)'
