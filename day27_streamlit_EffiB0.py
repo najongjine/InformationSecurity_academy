eaf17884b0b037cd222de21715c904b1088557a0
najongjine <najongjin3@hotmail.com>
2025-07-17 15:27:17 +0900

    .

--- day27_streamlit_EffiB0.py
+++ day27_streamlit_EffiB0.py
@@ -27,3 +27,28 @@ if uploaded_file is not None:
     st.markdown("### 🔢 클래스별 확률")
     for i, prob in enumerate(pred_probs):
         st.write(f"{class_names[i]}: {prob:.4f}")
+
+
+
+"""
+이미지 분류기 하면서 
+google colab,
+google drive,
+hugging face,
+streamlit
+썻어요.
+
+학습{
+ google colab- 학습할 코드 적는곳. 예측 테스트코드 적는곳.
+ google drive- 학습시킬 파일들, 학습끝난 모델 저장
+}
+
+모델 공개{
+ hugging face - 코드에서 모델 지가 알아서 다운로드 받아서 쓸수 있게 해줌
+}
+
+streamlit{
+ 웹사이트 아님!!! 이걸로 고객사 배포하면 큰일남
+ 포트폴리오 공개 할려고 올리는곳임
+}
+"""
