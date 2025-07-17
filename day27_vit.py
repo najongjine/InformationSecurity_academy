77b1f697ff364d341daa6fc69e63db8437b95397
najongjine <najongjin3@hotmail.com>
2025-07-17 15:48:01 +0900

    .

--- day27_vit.py
+++ day27_vit.py
@@ -3,3 +3,9 @@
 
 pip install -U datasets huggingface_hub fsspec
 """
+
+# 🟢 2. 필요한 라이브러리 불러오기
+from transformers import ViTForImageClassification, ViTFeatureExtractor, TrainingArguments, Trainer
+import torch
+from torch.utils.data import DataLoader
+from transformers import ViTForImageClassification, ViTFeatureExtractor
