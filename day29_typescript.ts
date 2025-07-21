e51e3213d8259b5b438fbbac50daeaf6078a5f0a
najongjine <najongjin3@hotmail.com>
2025-07-21 15:13:51 +0900

    .

--- day29_typescript.ts
+++ day29_typescript.ts
@@ -9,3 +9,12 @@ OpenAI 에 계신 3000천명의 박사님들이 로또 긁듯히 해결해야함
 결과가 잘못 나오면 100% 사람책임
 그래서 기본기가 아주 중요
  */
+
+/**
+실행방법:
+tsx 파일이름.ts
+ */
+// test1 = "string 이에요" <- 파이썬
+let test1 = "string 이에요"
+let test2: string = "저도 string 이에요"
+console.log(`test1: ${test1}`)
