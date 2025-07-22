b4f74a045b7cc1cf97a1adf093fb459eeed584cc
najongjine <najongjin3@hotmail.com>
2025-07-22 15:49:43 +0900

    .

--- day30_typescript.ts
+++ day30_typescript.ts
@@ -16,11 +16,32 @@ const testfunc2 = (imageBlob: Blob) => {
      */
 }
 
-let x: any;
-testfunc1(x)
+/**
+ * add_test1 이라는 함수를 만듬.
+ * _b 와 _c 라는 매개면수를 받음
+ * 리턴은 number
+ */
+const add_test1 = (_b: number, _c: number) => {
+    let x = 1;
+    x = 2
+    x = 3
+    return _b + _c;
+}
 
 /**
  더하기를 수행하는 함수를 만들어 보세요.
  함수 이름은 add_test1
  매개변수는 2개 받기
+ */
+let x = 1;
+let b: number = 1;
+let c: number = 2;
+let sum = add_test1(b, c);
+x = 1
+x = 2
+/** Scope
+메인 스코프와, 함수 스코프, 함수안에 또 함수 스코프, 
+즉, 부모스코프와 자식 스코프가 있어요
+메인 스코프에서 add_test1 에 있는 x 는 접근 못해요
+쉽게 이해하면, 그냥 x 와, 함수안에 있는 x는 집주소가 달라요
  */
