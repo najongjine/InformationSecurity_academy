d726e726d605bb4bbfe813f93eae47471053c8db
najongjine <najongjin3@hotmail.com>
2025-07-30 14:18:05 +0900

    .

--- day36_fastapi_app.py
+++ day36_fastapi_app.py
@@ -1,4 +1,18 @@
 """
+파이썬 서버를 application(돈거래, 데이터 적재) 이런게 안쓰는이유
+1. 설치해야할 모듈들이 너무 많고, 표준화 되지도 않음
+2. 간단한 SELECT 코드도 직관이지 않고 어려움.
+   특히 데이터가 어떻게 생겼는지 알려주질 않으니,
+   GPT랑 에러 안나는 코드 찾을때까지 어마어마한 노가다가 들어감
+3. AI module 들이 무거우니, CI/CD 가 안됨.
+   CI/CD 뜻: 서버 점검 없이 재시작 1초만에 끝
+
+실전에선
+[SpringBoot, .NetCore, Nodejs]
+얘네들이 마스터 서버가 되서, DB 관리, 사용자 요청 처리
+왠만한거 다 하고, 특수 AI 기능만, 파이썬 서버와 소통해요
+
+
 pip install fastapi
 pip install "uvicorn[standard]"
 pip install sqlalchemy psycopg2 
