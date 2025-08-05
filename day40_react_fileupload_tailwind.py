fe9878cc611c30773d048be1813d9eb29691f512
najongjine <najongjin3@hotmail.com>
2025-08-05 14:17:24 +0900

    .

--- day40_react_fileupload_tailwind.py
+++ day40_react_fileupload_tailwind.py
@@ -11,3 +11,36 @@ CREATE TABLE t_files (
     file_size BIGINT DEFAULT 0,                       -- 파일 크기 (바이트 단위)
     created_at TIMESTAMP DEFAULT NOW()    -- 업로드 시간
 );
+
+"""
+## images: [
+  File {
+    size: 2502,       
+    type: 'image/png',
+    name: '4.png',
+    lastModified: 1754371016609
+  },
+  File {
+    size: 3268,
+    type: 'image/png',
+    name: '8_black.png',
+    lastModified: 1754371016610
+  }
+]
+## imageUrlList:  [
+  {
+    original_name: '4.png',
+    stored_name: '9d98d25a-1a42-4b2c-8e61-88b6536b294c.png',
+    file_path: 'C:\\Users\\itg\\Documents\\Hono\\Hono_test1_academy\\uploads\\9d98d25a-1a42-4b2c-8e61-88b6536b294c.png',
+    mime_type: 'image/png',
+    file_size: 2502
+  },
+  {
+    original_name: '8_black.png',
+    stored_name: '59db7d34-b6e3-4bcd-86e3-5aea547751c2.png',
+    file_path: 'C:\\Users\\itg\\Documents\\Hono\\Hono_test1_academy\\uploads\\59db7d34-b6e3-4bcd-86e3-5aea547751c2.png',
+    mime_type: 'image/png',
+    file_size: 3268
+  }
+]
+"""
