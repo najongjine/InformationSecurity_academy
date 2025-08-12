d13756871883bfd913db2723ce738db533e4e320
najongjine <najongjin3@hotmail.com>
2025-08-12 13:11:54 +0900

    .

---
+++ day45_gptoss20b_finetuning_code_rag.py
@@ -0,0 +1,7 @@
+"""
+https://colab.research.google.com/drive/1wi0tV1whDlU1FH2bNZ5P3P_NkkHQPY-U
+- gpt-oss-20b 파인튜닝 colab 코드
+
+https://colab.research.google.com/drive/1lOjq8E0Iihl3JfB1QieO4dbow9VnqpfJ?usp=sharing
+- gpt-oss-20b 사용예제 colab 코드(T4 에선 메모리 이슈로 안돌아감)
+"""
