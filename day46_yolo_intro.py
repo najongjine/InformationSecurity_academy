a629b0997817f1743054266485e74194a835e088
najongjine <najongjin3@hotmail.com>
2025-08-13 15:09:10 +0900

    .

--- day46_yolo_intro.py
+++ day46_yolo_intro.py
@@ -1,4 +1,7 @@
 """
 https://colab.research.google.com/drive/1Z7bH0R2vVPCP72CbOkBsxw8Zt61vFgBN?usp=sharing
 - yolo default model use
+
+https://colab.research.google.com/drive/1weSNcQkNJL915ObhH1_bj14RPB4mhBLU?usp=sharing
+- yolo finetuning colab code
 """
