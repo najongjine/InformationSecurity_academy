35c4277b2e54ef75be77b9705a165162f67f6e8b
najongjine <najongjin3@hotmail.com>
2025-08-13 15:18:17 +0900

    .

--- day46_yolo_intro.py
+++ day46_yolo_intro.py
@@ -4,4 +4,7 @@ https://colab.research.google.com/drive/1Z7bH0R2vVPCP72CbOkBsxw8Zt61vFgBN?usp=sh
 
 https://colab.research.google.com/drive/1weSNcQkNJL915ObhH1_bj14RPB4mhBLU?usp=sharing
 - yolo finetuning colab code
+
+https://universe.roboflow.com/ds/wnbmmv0Wr5?key=TIfN8mV99Z
+- 과일 데이터 129개
 """
