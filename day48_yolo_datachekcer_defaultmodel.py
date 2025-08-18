f5d753e8424689c044b0da70828c0139b73c9bd0
najongjine <najongjin3@hotmail.com>
2025-08-18 14:30:33 +0900

    .

--- day48_yolo_datachekcer_defaultmodel.py
+++ day48_yolo_datachekcer_defaultmodel.py
@@ -4,4 +4,11 @@ https://colab.research.google.com/drive/1Z7bH0R2vVPCP72CbOkBsxw8Zt61vFgBN?usp=sh
 
 https://colab.research.google.com/drive/1weSNcQkNJL915ObhH1_bj14RPB4mhBLU
 - yolo colab에서 파인튜닝하기
+
+yolo 기본 모델은, 차 사람 이정도밖에 판별을 못해요.
+그래서 파인튜닝이 필요해요
+
+
+https://universe.roboflow.com/ds/SFctUXTZuN?key=QycknQW80l
+- insect dataset
 """
