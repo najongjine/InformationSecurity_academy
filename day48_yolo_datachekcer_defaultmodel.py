2c9ebadefe262321da2dc0b8a668d78e7bf1211c
najongjine <najongjin3@hotmail.com>
2025-08-18 14:14:11 +0900

    .

---
+++ day48_yolo_datachekcer_defaultmodel.py
@@ -0,0 +1,7 @@
+"""
+https://colab.research.google.com/drive/1Z7bH0R2vVPCP72CbOkBsxw8Zt61vFgBN?usp=sharing
+- yolo 기본모델 사용
+
+https://colab.research.google.com/drive/1weSNcQkNJL915ObhH1_bj14RPB4mhBLU
+- yolo colab에서 파인튜닝하기
+"""
