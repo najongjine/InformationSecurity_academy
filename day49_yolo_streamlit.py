38ba7a724fa8d25ba4fcfd17239c46bce0fb3c5b
najongjine <najongjin3@hotmail.com>
2025-08-19 13:40:53 +0900

    .

--- day49_yolo_streamlit.py
+++ day49_yolo_streamlit.py
@@ -4,4 +4,16 @@ https://github.com/najongjine/yolo_streamlit
 
 https://github.com/najongjine/yolo_ultralystic
 - yolo gpu 학습코드, 샘플 모델들
+
+pip install uv
+
+uv init
+
+uv python install 3.10
+uv python pin 3.10
+uv venv --python 3.10
+
+uv add -r requirements.txt
+
+uv run streamlit run predict_streamlit.py
 """
