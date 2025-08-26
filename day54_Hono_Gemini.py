e1adfecb4ce475d3172aec0fe86a7b43d198fb22
najongjine <najongjin3@hotmail.com>
2025-08-26 13:47:20 +0900

    .

--- day54_Hono_Gemini.py
+++ day54_Hono_Gemini.py
@@ -1,5 +1,10 @@
 """
+npm i cross-env dotenv pg axios reflect-metadata rimraf tsx @neondatabase/serverless
+
+npm i -d ts-node
+
 https://chatgpt.com/share/68ad389e-16dc-8001-a6ca-f7c78088b29e
+
 
 """
 
